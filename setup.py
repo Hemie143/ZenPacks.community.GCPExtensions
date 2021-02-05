2caeedf13acdf58119cef3cf9b1381671f2dcc98
hemie143 <hemie143@gmail.com>
2021-02-05 10:04:36 +0100

    Corrected the scale on the threshold and event transform of cache hit ratio of Memory Store.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.GCPExtensions"
-VERSION = "1.4.2"
+VERSION = "1.4.3"
 AUTHOR = "Laurent Hemeryck"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
